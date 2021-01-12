eeb1f22bde7bb25f4ca4fad60a91a717cf58ecf1
jose monroy <jmonroy@huskers.unl.edu>
2021-01-12 21:59:33 +0100

    move from slc6 to slc7

---
+++ condor/submitAllGridpacksLxplus.sh
@@ -0,0 +1,9 @@
+a proper script has to be written
+
+
+
+./submitPileupLxplus.py iDM_Mchi-100p5_dMchi-1p0_mZDinput-300p0_ctau-0_1or2jets_icckw1_drjj0_xptj80_xqcut20_slc7_amd64_gcc630_CMSSW_9_3_16_tarball.tar.xz 2018 100
+./submitPileupLxplus.py iDM_Mchi-44p0_dMchi-8p0_mZDinput-120p0_ctau-0_1or2jets_icckw1_drjj0_xptj80_xqcut20_slc7_amd64_gcc630_CMSSW_9_3_16_tarball.tar.xz  2018 100
+./submitPileupLxplus.py iDM_Mchi-60p3_dMchi-0p6_mZDinput-180p0_ctau-0_1or2jets_icckw1_drjj0_xptj80_xqcut20_slc7_amd64_gcc630_CMSSW_9_3_16_tarball.tar.xz  2018 100
+./submitPileupLxplus.py iDM_Mchi-80p4_dMchi-0p8_mZDinput-240p0_ctau-0_1or2jets_icckw1_drjj0_xptj80_xqcut20_slc7_amd64_gcc630_CMSSW_9_3_16_tarball.tar.xz  2018 100
+./submitPileupLxplus.py iDM_Mchi-88p0_dMchi-16p0_mZDinput-240p0_ctau-0_1or2jets_icckw1_drjj0_xptj80_xqcut20_slc7_amd64_gcc630_CMSSW_9_3_16_tarball.tar.xz 2018 100
